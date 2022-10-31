boundarydevices/linux

commit a31678d546aa18fb0a4be3833994a4daf3971665

drm/imx: Replace reset flow for DSIM
After the dispmix reset driver development, the reset flow
is better to be replaced by using this driver to hide the
reset details for all the dispmix submodules, and whose
driver code for reset can become platform independent.

Signed-off-by: Fancy Fang <chen.fang@nxp.com>
(cherry picked from commit b1fd6e40a4d013269c48fb223dd914ba09a168ee)

drivers/gpu/drm/imx/sec_mipi_dsim-imx.c
@@ -24,7 +24,7 @@
 #include <linux/mfd/syscon.h>
 #include <linux/platform_device.h>
 #include <linux/pm_runtime.h>
-#include <linux/regmap.h>
+#include <linux/reset.h>
 #include <drm/bridge/sec_mipi_dsim.h>
 #include <drm/drm_bridge.h>
 #include <drm/drm_encoder.h>
@@ -36,21 +36,13 @@
 
 #define DRIVER_NAME "imx_sec_dsim_drv"
 
-/* Dispmix Control & GPR Registers */
-#define DISPLAY_MIX_SFT_RSTN_CSR		0X00
-   #define MIPI_DSI_I_PRESETn_SFT_EN		BIT(5)
-#define DISPLAY_MIX_CLK_EN_CSR			0x04
-   #define MIPI_DSI_PCLK_SFT_EN			BIT(8)
-   #define MIPI_DSI_CLKREF_SFT_EN		BIT(9)
-#define GPR_MIPI_RESET_DIV			0x08
-   /* Clock & Data lanes reset: Active Low */
-   #define GPR_MIPI_S_RESETN			BIT(16)
-   #define GPR_MIPI_M_RESETN			BIT(17)
-
 struct imx_sec_dsim_device {
 	struct device *dev;
 	struct drm_encoder encoder;
-	struct regmap *gpr;
+
+	struct reset_control *soft_resetn;
+	struct reset_control *clk_enable;
+	struct reset_control *mipi_reset;
 
 	atomic_t rpm_suspended;
 };
@@ -73,59 +65,39 @@ static int imx_sec_dsim_runtime_resume(struct device *dev)
 }
 #endif
 
-static void disp_mix_dsim_soft_reset_release(struct regmap *gpr, bool release)
+static int sec_dsim_rstc_reset(struct reset_control *rstc, bool assert)
 {
-	if (release)
-		/* release dsi blk reset */
-		regmap_update_bits(gpr, DISPLAY_MIX_SFT_RSTN_CSR,
-				   MIPI_DSI_I_PRESETn_SFT_EN,
-				   MIPI_DSI_I_PRESETn_SFT_EN);
-	else
-		regmap_update_bits(gpr, DISPLAY_MIX_SFT_RSTN_CSR,
-				   MIPI_DSI_I_PRESETn_SFT_EN,
-				   0x0);
-}
+	int ret;
 
-static void disp_mix_dsim_clks_enable(struct regmap *gpr, bool enable)
-{
-	if (enable)
-		regmap_update_bits(gpr, DISPLAY_MIX_CLK_EN_CSR,
-				   MIPI_DSI_PCLK_SFT_EN | MIPI_DSI_CLKREF_SFT_EN,
-				   MIPI_DSI_PCLK_SFT_EN | MIPI_DSI_CLKREF_SFT_EN);
-	else
-		regmap_update_bits(gpr, DISPLAY_MIX_CLK_EN_CSR,
-				   MIPI_DSI_PCLK_SFT_EN | MIPI_DSI_CLKREF_SFT_EN,
-				   0x0);
-}
+	if (!rstc)
+		return 0;
 
-static void imx_sec_dsim_lanes_reset(struct regmap *gpr, bool reset)
-{
-	if (!reset)
-		/* release lanes reset */
-		regmap_update_bits(gpr, GPR_MIPI_RESET_DIV,
-				   GPR_MIPI_M_RESETN,
-				   GPR_MIPI_M_RESETN);
-	else
-		/* reset lanes */
-		regmap_update_bits(gpr, GPR_MIPI_RESET_DIV,
-				   GPR_MIPI_M_RESETN,
-				   0x0);
+	ret = assert ? reset_control_assert(rstc)	:
+		       reset_control_deassert(rstc);
+
+	return ret;
 }
 
 static void imx_sec_dsim_encoder_helper_enable(struct drm_encoder *encoder)
 {
+	int ret;
 	struct imx_sec_dsim_device *dsim_dev = enc_to_dsim(encoder);
 
 	pm_runtime_get_sync(dsim_dev->dev);
 
-	imx_sec_dsim_lanes_reset(dsim_dev->gpr, false);
+	ret = sec_dsim_rstc_reset(dsim_dev->mipi_reset, false);
+	if (ret)
+		dev_err(dsim_dev->dev, "deassert mipi_reset failed\n");
 }
 
 static void imx_sec_dsim_encoder_helper_disable(struct drm_encoder *encoder)
 {
+	int ret;
 	struct imx_sec_dsim_device *dsim_dev = enc_to_dsim(encoder);
 
-	imx_sec_dsim_lanes_reset(dsim_dev->gpr, true);
+	ret = sec_dsim_rstc_reset(dsim_dev->mipi_reset, true);
+	if (ret)
+		dev_err(dsim_dev->dev, "deassert mipi_reset failed\n");
 
 	pm_runtime_put_sync(dsim_dev->dev);
 }
@@ -214,6 +186,54 @@ static const struct of_device_id imx_sec_dsim_dt_ids[] = {
 };
 MODULE_DEVICE_TABLE(of, imx_sec_dsim_dt_ids);
 
+static int sec_dsim_of_parse_resets(struct imx_sec_dsim_device *dsim)
+{
+	int ret;
+	struct device *dev = dsim->dev;
+	struct device_node *np = dev->of_node;
+	struct device_node *parent, *child;
+	struct of_phandle_args args;
+	struct reset_control *rstc;
+	const char *compat;
+	uint32_t len, rstc_num = 0;
+
+	ret = of_parse_phandle_with_args(np, "resets", "#reset-cells",
+					 0, &args);
+	if (ret)
+		return ret;
+
+	parent = args.np;
+	for_each_child_of_node(parent, child) {
+		compat = of_get_property(child, "compatible", NULL);
+		if (!compat)
+			continue;
+
+		rstc = of_reset_control_array_get(child, false, false, true);
+		if (IS_ERR(rstc))
+			continue;
+
+		len = strlen(compat);
+		if (!of_compat_cmp("dsi,soft-resetn", compat, len)) {
+			dsim->soft_resetn = rstc;
+			rstc_num++;
+		} else if (!of_compat_cmp("dsi,clk-enable", compat, len)) {
+			dsim->clk_enable = rstc;
+			rstc_num++;
+		} else if (!of_compat_cmp("dsi,mipi-reset", compat, len)) {
+			dsim->mipi_reset = rstc;
+			rstc_num++;
+		} else
+			dev_warn(dev, "invalid dsim reset node: %s\n", compat);
+	}
+
+	if (!rstc_num) {
+		dev_err(dev, "no invalid reset control exists\n");
+		return -EINVAL;
+	}
+
+	return 0;
+}
+
 static int imx_sec_dsim_bind(struct device *dev, struct device *master,
 			     void *data)
 {
@@ -244,9 +264,9 @@ static int imx_sec_dsim_bind(struct device *dev, struct device *master,
 	if (irq < 0)
 		return -ENODEV;
 
-	dsim_dev->gpr = syscon_regmap_lookup_by_phandle(np, "dsi-gpr");
-	if (IS_ERR(dsim_dev->gpr))
-		return PTR_ERR(dsim_dev->gpr);
+	ret = sec_dsim_of_parse_resets(dsim_dev);
+	if (ret)
+		return ret;
 
 	encoder = &dsim_dev->encoder;
 	ret = imx_drm_encoder_parse_of(drm_dev, encoder, np);
@@ -334,9 +354,7 @@ static int imx_sec_dsim_runtime_suspend(struct device *dev)
 
 static int imx_sec_dsim_runtime_resume(struct device *dev)
 {
-	const struct of_device_id *of_id;
-	const char *compatible;
-	struct regmap *gpr = dsim_dev->gpr;
+	int ret;
 
 	if (unlikely(!atomic_read(&dsim_dev->rpm_suspended))) {
 		dev_warn(dsim_dev->dev,
@@ -349,23 +367,23 @@ static int imx_sec_dsim_runtime_resume(struct device *dev)
 
 	request_bus_freq(BUS_FREQ_HIGH);
 
-	/* Pull dsim out of reset */
-	of_id = of_match_device(imx_sec_dsim_dt_ids, dev);
-	compatible = of_id->compatible;
-
-	if (!strcmp(compatible, "fsl,imx8mn-mipi-dsim")) {
-		regmap_update_bits(gpr, DISPLAY_MIX_SFT_RSTN_CSR,
-				   BIT(1) | BIT(0),
-				   BIT(1) | BIT(0));
-		regmap_update_bits(gpr, DISPLAY_MIX_CLK_EN_CSR,
-				   BIT(1) | BIT(0),
-				   BIT(1) | BIT(0));
-	} else {
-		disp_mix_dsim_soft_reset_release(dsim_dev->gpr, true);
-		disp_mix_dsim_clks_enable(dsim_dev->gpr, true);
+	ret = sec_dsim_rstc_reset(dsim_dev->soft_resetn, false);
+	if (ret) {
+		dev_err(dev, "deassert soft_resetn failed\n");
+		return ret;
 	}
 
-	imx_sec_dsim_lanes_reset(dsim_dev->gpr, false);
+	ret = sec_dsim_rstc_reset(dsim_dev->clk_enable, true);
+	if (ret) {
+		dev_err(dev, "assert clk_enable failed\n");
+		return ret;
+	}
+
+	ret = sec_dsim_rstc_reset(dsim_dev->mipi_reset, false);
+	if (ret) {
+		dev_err(dev, "deassert mipi_reset failed\n");
+		return ret;
+	}
 
 	sec_mipi_dsim_resume(dev);
 

MrCry0/linux-fslc

commit 094e0ec7a3f854116fa4383d2db95ccfcdd2383b

[FIO toup] gpu: drm: imx: sec_mipi_dsim-imx: fix exit path while probing
The reset controls of DSIM module treat as exclusive, so they should
be released if the probing fails. Otherwise, all other probing always
fail and throw a kernel warning [1].

[1]
  [    2.275283] WARNING: CPU: 0 PID: 90 at drivers/reset/core.c:765 __reset_control_get_internal+0x150/0x170
  [    2.298872] Modules linked in:
  [    2.301936] CPU: 0 PID: 90 Comm: kworker/u8:2 Not tainted 5.15.60-lmp-standard #1
  [    2.309430] Hardware name: FSL i.MX8MM EVK board (DT)
  [    2.314489] Workqueue: events_unbound deferred_probe_work_func
  [    2.320336] pstate: 20000005 (nzCv daif -PAN -UAO -TCO -DIT -SSBS BTYPE=--)
  [    2.327306] pc : __reset_control_get_internal+0x150/0x170
  [    2.332713] lr : __of_reset_control_get+0x178/0x1d4
  [    2.337597] sp : ffffffc009dab980
  [    2.340915] x29: ffffffc009dab980 x28: ffffff8055c08bf0 x27: ffffff800591ce38
  [    2.348065] x26: 0000000000000001 x25: 0000000000000000 x24: 0000000000000001
  [    2.355218] x23: 0000000000000000 x22: ffffff8004db9480 x21: 0000000000000005
  [    2.362371] x20: ffffff8004db94a0 x19: ffffff80059cbe80 x18: ffffffffffffffff
  [    2.369521] x17: 3030303361303340 x16: 6332692f30303030 x15: 0100000000000041
  [    2.376672] x14: ffffffffffffffff x13: 0000000000000038 x12: 0101010101010101
  [    2.383826] x11: 0000000000000000 x10: 0101010101010101 x9 : ffffffc00873c038
  [    2.390977] x8 : 7f7f7f7f7f7f7f7f x7 : 64622c6c766f21ff x6 : 802370771f481000
  [    2.397193] imx6q-pcie 33800000.pcie: iATU unroll: enabled
  [    2.398128] x5 : fffffffdfdc09d1c x4 : 0000000000000000 x3 : 0000000000000001
  [    2.403639] imx6q-pcie 33800000.pcie: Detected iATU regions: 4 outbound, 4 inbound
  [    2.410764] x2 : 0000000000000005 x1 : 0000000000000005 x0 : 0000000000000000
  [    2.418364] imx6q-pcie 33800000.pcie: host bridge /soc@0/pcie@33800000 ranges:
  [    2.425482] Call trace:
  [    2.425485]  __reset_control_get_internal+0x150/0x170
  [    2.432762] imx6q-pcie 33800000.pcie:       IO 0x001ff80000..0x001ff8ffff -> 0x0000000000
  [    2.435158]  __of_reset_control_get+0x178/0x1d4
  [    2.435164]  of_reset_control_array_get+0xb8/0x230
  [    2.440246] imx6q-pcie 33800000.pcie:      MEM 0x0018000000..0x001fefffff -> 0x0018000000
* [    2.448397]  imx_sec_dsim_probe+0x23c/0x360
  [    2.453062] imx6q-pcie 33800000.pcie: iATU unroll: enabled
  [    2.457725]  platform_probe+0x74/0xf0
  [    2.457734]  really_probe+0xd4/0x474
  [    2.465931] imx6q-pcie 33800000.pcie: Detected iATU regions: 4 outbound, 4 inbound
  [    2.470096]  __driver_probe_device+0x98/0x18c
  [    2.470104]  driver_probe_device+0x48/0xe0
  [    2.470111]  __device_attach_driver+0xb8/0x144
  [    2.503299]  bus_for_each_drv+0x84/0xe0
  [    2.507143]  __device_attach+0xb0/0x1f4
  [    2.510989]  device_initial_probe+0x20/0x30
  [    2.515185]  bus_probe_device+0xa8/0xb0
  [    2.519031]  deferred_probe_work_func+0xa8/0xfc
  [    2.523570]  process_one_work+0x220/0x4bc
  [    2.527589]  worker_thread+0x2e8/0x464
  [    2.531347]  kthread+0x148/0x160
  [    2.534583]  ret_from_fork+0x10/0x20
  [    2.538165] ---[ end trace 34aac26f257307ff ]---
  [    2.543033] ------------[ cut here ]------------

Fixes: a31678d546aa1 ("drm/imx: Replace reset flow for DSIM")

drivers/gpu/drm/imx/sec_mipi_dsim-imx.c
@@ -456,7 +456,11 @@ static int imx_sec_dsim_probe(struct platform_device *pdev)
 
 	pm_runtime_enable(dev);
 
-	return component_add(dev, &imx_sec_dsim_ops);
+	ret = component_add(dev, &imx_sec_dsim_ops);
+	if (ret)
+		sec_dsim_of_put_resets(dsim_dev);
+
+	return ret;
 }
 
 static int imx_sec_dsim_remove(struct platform_device *pdev)